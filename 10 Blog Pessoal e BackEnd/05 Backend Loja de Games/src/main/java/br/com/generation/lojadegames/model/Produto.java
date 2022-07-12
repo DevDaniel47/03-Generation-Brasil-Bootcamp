fdc459681013f72c48e88a90527e935e8571701a
DevDaniel47 <daniel.oliveira0622@gmail.com>
2022-07-12 14:54:39 -0300

    Adição do novo projeto (Backend Loja de Games) e Incrementação do CRUD

---
+++ 10 Blog Pessoal e BackEnd/05 Backend Loja de Games/src/main/java/br/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,77 @@
+package br.com.generation.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 1, max = 3)
+	private int idade;
+	
+	@NotNull
+	@Size(min = 15, max = 255)
+	private String descricao;
+	
+	@NotNull
+	private Double tamanho;
+	
+	@NotNull
+	private Double valor;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(Double tamanho) {
+		this.tamanho = tamanho;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+
+	
+}
